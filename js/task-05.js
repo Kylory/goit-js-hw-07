5e075199748ec51abb679a5d3d10a492a9772045
Kylory <72569852+Kylory@users.noreply.github.com>
2021-04-08 21:19:27 +0300

    refactored task-05

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,9 @@ const inputRef = document.querySelector("#name-input");
 const outputRef = document.querySelector("#name-output");
 
 const magic = () => {
-  if (inputRef.value) {
-    outputRef.textContent = inputRef.value;
-  } else outputRef.textContent = "незнайомець";
+  inputRef.value
+    ? (outputRef.textContent = inputRef.value)
+    : (outputRef.textContent = "незнайомець");
 };
 
 inputRef.addEventListener("input", magic);
